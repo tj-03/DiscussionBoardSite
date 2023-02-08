3408be94b699096b720647d1463f4a4ecac18ae2
Anthony-Rebello <73561379+Anthony-Rebello@users.noreply.github.com>
2023-02-08 18:07:52 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	godotenv.Load("dev.env")
+}
+func main() {
+	server := NewServer(os.Getenv("DB_URL"))
+	//log to a file
+	log.Fatal(server.Run(os.Getenv("PORT")))
+}
