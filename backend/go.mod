39cc6e821eb1413386e0f65603b0ac271a9fd57d
Anthony-Rebello <a.rebello0417@gmail.com>
2023-02-08 18:23:38 -0500

    Updated with mongoDB functionality

--- backend/go.mod
+++ backend/go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	go.mongodb.org/mongo-driver v1.11.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func init() {
@@ -14,4 +20,25 @@ func main() {
 	server := NewServer(os.Getenv("DB_URL"))
 	//log to a file
 	log.Fatal(server.Run(os.Getenv("PORT")))
+
+	//Instantiates the client and connection location
+	client, err := mongo.NewClient(options.Client().ApplyURI(server))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Instantiates the context and connects the to the client
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Disconnects the client
+	defer client.Disconnect(ctx)
 }
