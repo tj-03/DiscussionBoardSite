ad87544ecb75abd5685b1e6230b0a57c9d783d03
Anthony-Rebello <a.rebello0417@gmail.com>
2023-02-08 20:48:23 -0500

    Update auth.go
    Added outline for user existence check.

--- backend/auth.go
+++ backend/auth.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"firebase.google.com/go/auth"
 	"github.com/gin-gonic/gin"
@@ -13,6 +20,36 @@ type FirebaseAuthProvider struct {
 
 func NewFirebaseAuthProvider(firebaseClient *auth.Client) FirebaseAuthProvider {
 	return FirebaseAuthProvider{FirebaseClient: firebaseClient}
+}
+
+
+//TODO: Change <UserIDType> to the UserID type
+func checkUserExists(UserID <UserIDType>) bool {
+	//Instantiates the client and connection location
+	client, err := mongo.NewClient(options.Client().ApplyURI(server))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Instantiates the context and connects the to the client
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	if client.Database("myDB").Collection("Users").Find(ctx, bson.M{"UserID": UserID}){
+		return true
+	}
+	else{
+		return false
+	}
 }
 
 func (ap *FirebaseAuthProvider) Middleware() gin.HandlerFunc {
@@ -30,6 +67,13 @@ func (ap *FirebaseAuthProvider) Middleware() gin.HandlerFunc {
 		}
 
 		//TODO: Check if user exists in DB
+		//See comments for checkUserExists()
+		//if checkUserExists(<UserID>){
+		//	authenticate users
+		//}
+		//else{
+		//	user does not exist	
+		//}
 
 		c.Next()
 	}
