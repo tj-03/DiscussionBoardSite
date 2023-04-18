4dcdd6b4ab0848ed81a12cad5a8bac4e473fefb4
alexopyrchal8 <66843881+alexopyrchal8@users.noreply.github.com>
2023-04-18 17:12:31 -0400

    Repositioning of about team page

---
+++ frontend/src/app/aboutteam/aboutteam/aboutteam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutteam',
+  templateUrl: './aboutteam.component.html',
+  styleUrls: ['./aboutteam.component.css']
+})
+export class AboutteamComponent {
+
+}
